a62495767c9e8d8673ff01346544db88b2d05865
earnest1997 <2692132992@qq.com>
2019-09-17 22:48:29 +0800

    ad

--- bin/index.js
+++ bin/index.js
@@ -35,14 +35,54 @@ class Spider {
     }
   }
 
-  async ceateReq() {
+  async searchArticle() {
   const browser=await puppeteer.launch({headless:true})
+  const jianshuSearch= new Promise(async(resolve,reject)=>{
   const page=await browser.newPage()
   await page.goto('https://www.jianshu.com/')
   await page.focus('.search-input')
   await page.type(this.keywords,{delay:100}) //输入变慢像一个用户
   await page.click('.search-btn')
-  const waitForElement=page.waitForSelector(".note-list")
+  try{
+  const waitForElement=await page.waitForSelector(".note-list")
+  const resultCount=waitForElement.length
+  const crawlCount=resultCount>=4?4:resultCount
+  Array.from({length:crawlCount}).map(async(item,index)=>{
+   const author=await page.$eval(`.note-list>li:nth-child[${index}] .nickname`,el=>el.innerHTML)
+  const time=await page.$eval(`.note-list>li:nth-child[${index}] .time`,el=>el.innerHTML)
+  const title=await page.$eval(`.note-list>li:nth-child[${index}] .title`,el=>el.innerHTML)
+  const description=await page.$eval(`.note-list>li:nth-child[${index}] .abstract`,el=>el.innerHTML)
+  const url=await page.$eval(`.note-list>li:nth-child[${index}] .title`,el=>el.href)
+  resolve({author,time,title,description,url})
+  })
+  }
+  catch(err){
+    console.log(err,'获取页面元素失败')
+    reject('获取页面元素失败')
+  }
+})
+const juejinSearch=new Promise(async(resolve,reject)=>{
+  const page=await browser.newPage()
+  await page.goto('https://juejin.im/')
+  await page.focus('.search-input')
+  await page.type(this.keywords,{delay:100}) //输入变慢像一个用户
+  await page.click('.search-icon')
+  const waitForElement=await page.waitForSelector(".main-list")
+  const resultCount=waitForElement.item(0).length
+  const childArr=Array.from(waitForElement.item(0).childNodes).filter(item=>item.innerHTML.test(/class="entry"/))
+
+  const crawlCount=resultCount>=4?4:resultCount
+childArr.length=crawlCount
+  childArr.map(async(item,index)=>{
+    const author=await page.$eval(`.main-list>li:nth-child[${index}] .nickname`,el=>el.innerHTML)
+   const time=await page.$eval(`.main-list>li:nth-child[${index}] .time`,el=>el.innerHTML)
+   const title=await page.$eval(`.main-list>li:nth-child[${index}] .title`,el=>el.innerHTML)
+   const description=await page.$eval(`.main-list>li:nth-child[${index}] .abstract`,el=>el.innerHTML)
+   const url=await page.$eval(`.main-list>li:nth-child[${index}] .title`,el=>el.href)
+   resolve({author,time,title,description,url})
+   })
+})
+
   }
 
   async crawl() {
